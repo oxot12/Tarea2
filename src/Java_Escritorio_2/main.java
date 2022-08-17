6039057c693578fd09d03474d22d4cbb5ccef508
unknown <aoseasx45@gmail.com>
2022-08-16 22:39:10 -0600

    Limpio1

---
+++ src/Java_Escritorio_2/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Java_Escritorio_2;
+
+/**
+ *
+ * @author Oseas Xt
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Cadenas_Murcielagos vista = new Cadenas_Murcielagos();
+        vista.show();
+    }
+}
